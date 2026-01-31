340c37f2556a3fec60260bf44a4ed09aa8025591
QB <h781220092@qq.com>
2026-01-31 22:23:50 +0800

    全局状态集中管理，替代入口局部状态

---
+++ static/js/modules/app_state.js
@@ -0,0 +1,8 @@
+﻿﻿﻿// 全局状态变量，保持与原入口一致
+var logRefreshInterval = null;
+var taskRefreshInterval = null;
+var resultsRefreshInterval = null;
+var lastResultsSignature = null;
+var latestTasks = [];
+var latestAccounts = [];
+var isTaskReordering = false;
